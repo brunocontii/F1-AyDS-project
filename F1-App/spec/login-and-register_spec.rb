6eb0d0ef4c0db66fd23cc05bcbc9cb5aa30b7066
Bruno Conti <brunocse.5@gmail.com>
2024-08-30 20:53:47 -0300

    adding test for GET /gamemodes

--- F1-App/spec/login-and-register_spec.rb
+++ F1-App/spec/login-and-register_spec.rb
@@ -166,7 +166,7 @@ RSpec.describe 'The App' do
                 env 'rack.session', { username: user.username }
                 profile
             end
-            
+
             it 'returns JSON with lives when requested via AJAX' do
                 env 'rack.session', { username: user.username }
                 get '/profile', {}, 'HTTP_X_REQUESTED_WITH' => 'XMLHttpRequest'
@@ -216,11 +216,50 @@ RSpec.describe 'The App' do
         end
 
         context 'when user is not logged in' do
-            it 'redirects to loggin page' do
+            it 'redirects to home page' do
             post '/profile/picture'
             expect(last_response).to be_redirect
             follow_redirect!
             expect(last_request.path).to eq('/')
+            end
+        end
+    end
+
+    describe 'GET /gamemodes' do
+        context 'when user is loggen in' do
+            let(:user) do
+                User.create(
+                    username: 'testuser',
+                    password: 'testpassword',
+                    cant_life: 3,
+                    cant_coins: 0
+                )
+            end
+
+            before do
+                env 'rack.session', { username: user.username }
+            end
+
+            it 'returns JSON with lives when requested via AJAX' do
+                env 'rack.session', { username: user.username }
+                get '/gamemodes', {}, 'HTTP_X_REQUESTED_WITH' => 'XMLHttpRequest'
+                expect(last_response.content_type).to eq('application/json')
+                expect(last_response.body).to eq({ lives: user.cant_life }.to_json)
+            end
+
+            it 'renders gamemodes template when not requested via AJAX' do
+                get '/gamemodes'
+                expect(last_response).to be_ok
+                expect(last_response.body).to include('Gamemodes')
+            end
+        end
+
+        context 'when user is not logged in' do
+            it 'redirects to home page' do
+                get '/gamemodes'
+                expect(last_response).to be_redirect
+                follow_redirect!
+                expect(last_request.path).to eq('/')
             end
         end
     end
